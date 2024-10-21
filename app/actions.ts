8bca941d05cef06cb9d338143f4ff2acce450b45
NeilRow56 <neilrowland56@gmail.com>
2024-10-21 08:47:53 +0100

    Availability page working with server action

--- app/actions.ts
+++ app/actions.ts
@@ -1,9 +1,12 @@
 "use server";
 
-import { redirect } from "next/navigation";
 import { parseWithZod } from "@conform-to/zod";
-import { aboutSettingsSchema, onboardingSchema } from "@/lib/zodSchemas";
+
+import { redirect } from "next/navigation";
+
+import { revalidatePath } from "next/cache";
 import { requireUser } from "@/lib/hooks";
+import { aboutSettingsSchema, onboardingSchema } from "@/lib/zodSchemas";
 import prisma from "@/lib/db";
 
 export async function onboardingAction(prevState: any, formData: FormData) {
@@ -28,17 +31,58 @@ export async function onboardingAction(prevState: any, formData: FormData) {
     return submission.reply();
   }
 
-  const data = await prisma.user.update({
+  const OnboardingData = await prisma.user.update({
     where: {
-      id: session?.user?.id,
+      id: session.user?.id,
     },
     data: {
       username: submission.value.username,
       name: submission.value.fullName,
+      Availability: {
+        createMany: {
+          data: [
+            {
+              day: "Monday",
+              fromTime: "08:00",
+              tillTime: "18:00",
+            },
+            {
+              day: "Tuesday",
+              fromTime: "08:00",
+              tillTime: "18:00",
+            },
+            {
+              day: "Wednesday",
+              fromTime: "08:00",
+              tillTime: "18:00",
+            },
+            {
+              day: "Thursday",
+              fromTime: "08:00",
+              tillTime: "18:00",
+            },
+            {
+              day: "Friday",
+              fromTime: "08:00",
+              tillTime: "18:00",
+            },
+            {
+              day: "Saturday",
+              fromTime: "08:00",
+              tillTime: "18:00",
+            },
+            {
+              day: "Sunday",
+              fromTime: "08:00",
+              tillTime: "18:00",
+            },
+          ],
+        },
+      },
     },
   });
 
-  redirect("/onboarding/grant-id");
+  return redirect("/onboarding/grant-id");
 }
 
 export async function SettingsAction(prevState: any, formData: FormData) {
@@ -63,4 +107,38 @@ export async function SettingsAction(prevState: any, formData: FormData) {
   });
 
   return redirect("/dashboard");
+}
+
+export async function updateAvailabilityAction(formData: FormData) {
+  const session = await requireUser();
+
+  const rawData = Object.fromEntries(formData.entries());
+  const availabilityData = Object.keys(rawData)
+    .filter((key) => key.startsWith("id-"))
+    .map((key) => {
+      const id = key.replace("id-", "");
+      return {
+        id,
+        isActive: rawData[`isActive-${id}`] === "on",
+        fromTime: rawData[`fromTime-${id}`] as string,
+        tillTime: rawData[`tillTime-${id}`] as string,
+      };
+    });
+
+  try {
+    await prisma.$transaction(
+      availabilityData.map((item) =>
+        prisma.availability.update({
+          where: { id: item.id },
+          data: {
+            isActive: item.isActive,
+            fromTime: item.fromTime,
+            tillTime: item.tillTime,
+          },
+        }),
+      ),
+    );
+  } catch (error) {
+    console.error("Error updating availability:", error);
+  }
 }
